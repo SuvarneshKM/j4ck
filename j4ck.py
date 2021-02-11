501052d4f4f5bbe394de543f18fd834bf1fa4935
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-02-11 22:21:44 +0530

    commit

--- j4ck.py
+++ j4ck.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
-@app.route('/user/<name>')
+# @app.route('/user/<name>')
 
 
-def user(name):
-		return "<h1>Hello {}!!</h1>".format(name)	
+# def user(name):
+# 		return "<h1>Hello {}!!</h1>".format(name)	
